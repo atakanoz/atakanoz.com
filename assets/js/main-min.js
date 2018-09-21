2ad41d6bdc9348364acbe98018080b09404ae93e
Atakan Öz <atakanozofficial@gmail.com>
2018-09-21 16:21:37 +0300

    Initial Release

---
+++ assets/js/main-min.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	tippy('.ao__about', {
+	  delay: 100,
+	  arrow: true,
+	  arrowType: 'round',
+	  size: 'large',
+	  duration: 500
+	})
+});
+
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+	tippy('.ao__about', {
+	  delay: 100,
+	  arrow: true,
+	  arrowType: 'round',
+	  size: 'large',
+	  duration: 500
+	})
+});
