88a74b802bf9098ebd6715ccc3b5f05617b0998d
Atakan Öz <atakanozofficial@gmail.com>
2022-10-01 02:15:37 +0300

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,88 @@
+export const SITE_NAV = {
+	items: [
+	{
+			name: "Home",
+			link: "/",
+		},
+		{
+			name: "Projects",
+			link: "/projects",
+		},
+		{
+			name: "About",
+			link: "/about",
+		},
+	],
+}
+
+export const SOCIAL_LINKS = {
+	items: [
+		{
+			name: "WordPress",
+			link: "/",
+		},
+		{
+			name: "Github",
+			link: "/",
+		},
+		{
+			name: "Codeable",
+			link: "/",
+		},
+		{
+			name: "Linkedin",
+			link: "/",
+		},
+		{
+			name: "Dribbble",
+			link: "/",
+		},
+		{
+			name: "Medium",
+			link: "/",
+		},
+	],
+}
+
+export const LINKS = {
+	navigation: [
+	{
+			name: "Home",
+			link: "/",
+		},
+		{
+			name: "Projects",
+			link: "/projects",
+		},
+		{
+			name: "About",
+			link: "/about",
+		},
+	],
+	social: [
+		{
+			name: "WordPress",
+			link: "/",
+		},
+		{
+			name: "Github",
+			link: "/",
+		},
+		{
+			name: "Codeable",
+			link: "/",
+		},
+		{
+			name: "Linkedin",
+			link: "/",
+		},
+		{
+			name: "Dribbble",
+			link: "/",
+		},
+		{
+			name: "Medium",
+			link: "/",
+		},
+	],
+}
