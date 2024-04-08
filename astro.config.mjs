0c53f6bb4ccc9f382a7492d26eb521b77194d332
Atakan Öz <atakanozofficial@gmail.com>
2024-04-08 18:17:21 +0800

    yarn update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import compress from "astro-compress";
 import robotsTxt from 'astro-robots-txt';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atakanoz.com',
-  experimental: {
-    integrations: true
-  },
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), compress(), sitemap(), robotsTxt()]
+	site: 'https://atakanoz.com',
+	experimental: {
+		integrations: true
+	},
+	integrations: [tailwind(), image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	}), sitemap(), robotsTxt()]
 });
